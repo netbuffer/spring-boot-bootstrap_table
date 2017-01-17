c0c42c03adeb7f20051384b247eb9a85c78b9a2a
zfy <gradle@foxmail.com>
2017-01-17 19:41:22 +0800

    add druid

--- src/main/java/cn/com/ttblog/SpringBootBootstrapTableApplication.java
+++ src/main/java/cn/com/ttblog/SpringBootBootstrapTableApplication.java
@@ -5,8 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringBootBootstrapTableApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootBootstrapTableApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootBootstrapTableApplication.class, args);
+    }
 }
