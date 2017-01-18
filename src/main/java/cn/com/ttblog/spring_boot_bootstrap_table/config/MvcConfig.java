58c28abe4742d5f2b6e01752aa465b257be7a6cc
netbuffer <javawiki@163.com>
2017-01-18 21:46:57 +0800

    test ContentNegotiatingViewResolver

--- src/main/java/cn/com/ttblog/spring_boot_bootstrap_table/config/MvcConfig.java
+++ src/main/java/cn/com/ttblog/spring_boot_bootstrap_table/config/MvcConfig.java
@@ -1,17 +1,47 @@
 package cn.com.ttblog.spring_boot_bootstrap_table.config;
 
+import cn.com.ttblog.spring_boot_bootstrap_table.views.JsonViewResolver;
+import com.alibaba.fastjson.support.spring.FastJsonJsonView;
+import org.springframework.beans.factory.BeanFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.accept.ContentNegotiationManager;
+import org.springframework.web.multipart.MultipartResolver;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.ViewResolver;
-import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.web.servlet.view.ContentNegotiatingViewResolver;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 @EnableWebMvc
 public class MvcConfig extends WebMvcConfigurerAdapter{
+
+    @Override
+    public void configureContentNegotiation(
+            ContentNegotiationConfigurer configurer) {
+        configurer.ignoreAcceptHeader(false).defaultContentType(MediaType.TEXT_HTML);
+    }
+
+    // 在此---配置ContentNegotiatingViewResolver,通过此代理到不同的viewResolover
+    @Bean
+    public ViewResolver contentNegotiatingViewResolver(
+            ContentNegotiationManager manager) {
+        ContentNegotiatingViewResolver resolver = new ContentNegotiatingViewResolver();
+        resolver.setContentNegotiationManager(manager);
+        List<ViewResolver> resolvers = new ArrayList<ViewResolver>();
+        // jsp view resolver
+        resolvers.add(internalResourceViewResolver());
+        JsonViewResolver jsonViewResolver=new JsonViewResolver();
+        resolvers.add(jsonViewResolver);
+        resolver.setViewResolvers(resolvers);
+        return resolver;
+    }
 
     @Bean
     public ViewResolver internalResourceViewResolver () {
@@ -20,6 +50,20 @@ public class MvcConfig extends WebMvcConfigurerAdapter{
         viewResolver.setSuffix(".jsp");
         viewResolver.setViewClass(JstlView.class);
         return viewResolver;
+    }
+
+    // 注册自定义拦截器
+//    @Override
+//    public void addInterceptors(InterceptorRegistry registry) {
+//        registry.addInterceptor();
+//    }
+
+    // 文件上传设置
+    @Bean
+    public MultipartResolver multipartResolver() {
+        CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver();
+        multipartResolver.setMaxUploadSize(1000000);
+        return multipartResolver;
     }
 
     @Override
