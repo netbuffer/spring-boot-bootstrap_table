70dd09536e290a1d42983aca800381e5403173e8
netbuffer <javawiki@163.com>
2017-05-09 18:17:59 +0800

    init thymeleaf branch

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 ### spring-boot bootstrap_table demo
 ---
 * spring-boot 1.4.3
-* integration with jsp
+* integration with thymeleaf
 * bootstrap-table1.9
 * bootstrap3  
 * log4jdbc-log4j2
--- src/main/java/cn/com/ttblog/spring_boot_bootstrap_table/controller/TestController.java
+++ src/main/java/cn/com/ttblog/spring_boot_bootstrap_table/controller/TestController.java
@@ -1,10 +1,14 @@
 package cn.com.ttblog.spring_boot_bootstrap_table.controller;
 
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -29,6 +33,17 @@ public class TestController {
 		//退出当前jvm，导致容器停止(tomcat关闭等)
 		System.exit(-1);
 		return "exit";
+	}
+
+	/**
+	 * 获取系统属性
+	 * @param prop
+	 * @return
+	 */
+	@RequestMapping("/prop")
+	@ResponseBody
+	public String getProperties(@RequestParam(value = "prop",required = false) String prop){
+		return StringUtils.isBlank(prop)?"":System.getProperty(prop);
 	}
 
 //	@ExceptionHandler
