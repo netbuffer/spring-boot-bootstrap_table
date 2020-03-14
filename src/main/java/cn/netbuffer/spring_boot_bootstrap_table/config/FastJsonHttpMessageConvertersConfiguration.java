9398066570b54010e657dee33a6e4485a8512a0d
netbuffer <javawiki@163.com>
2020-03-14 22:35:35 +0800

    adapt spring-boot2.x

--- src/main/java/cn/netbuffer/spring_boot_bootstrap_table/config/FastJsonHttpMessageConvertersConfiguration.java
+++ src/main/java/cn/netbuffer/spring_boot_bootstrap_table/config/FastJsonHttpMessageConvertersConfiguration.java
@@ -5,19 +5,18 @@ import com.alibaba.fastjson.serializer.SerializerFeature;
 import com.alibaba.fastjson.support.config.FastJsonConfig;
 import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
-import org.springframework.boot.autoconfigure.web.HttpMessageConverters;
+import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 
 @Configuration
-@ConditionalOnClass({JSON.class}) //1
+@ConditionalOnClass({JSON.class})
 public class FastJsonHttpMessageConvertersConfiguration {
 
     @Bean
     public HttpMessageConverters fastJsonHttpMessageConverters() {
         FastJsonHttpMessageConverter fastConverter = new FastJsonHttpMessageConverter();//2
-
         FastJsonConfig fastJsonConfig = new FastJsonConfig();
         fastJsonConfig.setSerializerFeatures(
                 SerializerFeature.PrettyFormat,
