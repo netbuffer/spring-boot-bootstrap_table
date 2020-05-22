9a6434316e57ee6ccb02b51fb4e969137943a29a
netbuffer <javawiki@163.com>
2020-05-22 22:48:13 +0800

    update validation

--- src/test/java/cn/netbuffer/validation/Book.java
+++ src/test/java/cn/netbuffer/validation/Book.java
@@ -3,16 +3,48 @@ package cn.netbuffer.validation;
 import lombok.Data;
 import org.hibernate.validator.constraints.Length;
 
-import javax.validation.constraints.NotNull;
+import javax.validation.GroupSequence;
+import javax.validation.Valid;
+import javax.validation.constraints.*;
+import javax.validation.groups.Default;
 import java.util.Date;
+import java.util.List;
 
 @Data
 public class Book {
 
-    @NotNull(message = "书籍名称不能为空")
+    /**
+     * 定义验证组
+     */
+    public interface AddBookGroup {
+    }
+
+    public interface UpdateBookGroup {
+    }
+
+    /**
+     * 组排序
+     */
+    @GroupSequence(value = {
+            AddBookGroup.class,
+            UpdateBookGroup.class,
+            Default.class
+    })
+    public interface Group {
+    }
+
+    @NotNull(message = "书籍名称不能为空", groups = {AddBookGroup.class})
     private String name;
+    @FutureOrPresent
     private Date createTime;
     @Length(min = 4, max = 20, message = "author字符长度受限")
     private String author;
-
+    @Min(value = 10)
+    @Max(value = 150)
+    private Integer authorAge;
+    @NotNull(message = "页码不能为空", groups = {AddBookGroup.class, UpdateBookGroup.class})
+    @Size(min = 1, max = Integer.MAX_VALUE, message = "页数非法")
+    private List<Integer> pages;
+    //级联验证
+    private List<@Valid Book> books;
 }
